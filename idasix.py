52ae36bc251d6c596d46080429459db677976d97
Nir <nirizr@gmail.com>
2017-04-13 00:44:11 +0300

    Add extra modules forgotten in the first place (#11)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idasix.py
+++ idasix.py
@@ -28,18 +28,20 @@ elif IDA_SDK_VERSION < 690:
   _ = QtCore
 
 
-modules = ['ida_idaapi', 'ida_auto', 'ida_dbg', 'ida_diskio', 'ida_entry',
-           'ida_enum', 'ida_expr', 'ida_fixup', 'ida_frame', 'ida_funcs',
-           'ida_gdl', 'ida_ida', 'ida_bytes', 'ida_idd', 'ida_idp',
-           'ida_kernwin', 'ida_lines', 'ida_loader', 'ida_moves', 'ida_nalt',
-           'ida_name', 'ida_netnode', 'ida_offset', 'ida_pro', 'ida_search',
-           'ida_segment', 'ida_srarea', 'ida_struct', 'ida_typeinf', 'ida_ua',
-           'ida_xref', 'ida_graph']
+modules_list = ['ida_allins', 'ida_area', 'ida_auto', 'ida_bytes', 'ida_dbg',
+                'ida_diskio', 'ida_entry', 'ida_enum', 'ida_expr', 'ida_fixup',
+                'ida_fpro', 'ida_frame', 'ida_funcs', 'ida_gdl', 'ida_graph',
+                'ida_hexrays', 'ida_ida', 'ida_idaapi', 'ida_idd', 'ida_idp',
+                'ida_ints', 'ida_kernwin', 'ida_lines', 'ida_loader',
+                'ida_moves', 'ida_nalt', 'ida_name', 'ida_netnode',
+                'ida_offset', 'ida_pro', 'ida_queue', 'ida_registry',
+                'ida_search', 'ida_segment', 'ida_srarea', 'ida_strlist',
+                'ida_struct', 'ida_typeinf', 'ida_ua', 'ida_xref']
 if IDA_SDK_VERSION >= 695:
   import ida_idaapi
   import ida_pro
   import ida_kernwin
-  for module in modules:
+  for module in modules_list:
     globals()[module] = __import__(module)
 elif IDA_SDK_VERSION < 695:
   import sys
@@ -47,7 +49,7 @@ elif IDA_SDK_VERSION < 695:
   ida_idaapi = idaapi
   ida_pro = idaapi
   ida_kernwin = idaapi
-  for module in modules:
+  for module in modules_list:
     sys.modules[module] = idaapi
 
 
