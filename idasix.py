8083c9710da28af6a076f1069434cad2cb913a7f
Nir <nirizr@gmail.com>
2017-04-27 13:10:36 +0300

    Fix all flake8 issues and remove flake8 config file (#13)
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

--- idasix.py
+++ idasix.py
@@ -1,13 +1,13 @@
 IDA_SDK_VERSION = None
 try:
-  import ida_pro
-  IDA_SDK_VERSION = ida_pro.IDA_SDK_VERSION
+    import ida_pro
+    IDA_SDK_VERSION = ida_pro.IDA_SDK_VERSION
 except ImportError:
-  import idaapi
-  IDA_SDK_VERSION = idaapi.IDA_SDK_VERSION
+    import idaapi
+    IDA_SDK_VERSION = idaapi.IDA_SDK_VERSION
 
 if not IDA_SDK_VERSION:
-  raise Exception("Couldn't figure out IDA version")
+    raise Exception("Couldn't figure out IDA version")
 
 # Handle different Qt versions. instead of:
 # 1. `from PySide import QtCore, QtGui` or
@@ -18,14 +18,14 @@ QtGui = None
 QtWidgets = None
 QtCore = None
 if IDA_SDK_VERSION >= 690:
-  # IDA version >= 6.9
-  from PyQt5 import QtCore, QtGui, QtWidgets
-  _ = QtCore
+    # IDA version >= 6.9
+    from PyQt5 import QtCore, QtGui, QtWidgets
+    _ = QtCore
 elif IDA_SDK_VERSION < 690:
-  # IDA version <= 6.8
-  from PySide import QtCore, QtGui
-  QtWidgets = QtGui
-  _ = QtCore
+    # IDA version <= 6.8
+    from PySide import QtCore, QtGui
+    QtWidgets = QtGui
+    _ = QtCore
 
 
 modules_list = ['ida_allins', 'ida_area', 'ida_auto', 'ida_bytes', 'ida_dbg',
@@ -38,80 +38,81 @@ modules_list = ['ida_allins', 'ida_area', 'ida_auto', 'ida_bytes', 'ida_dbg',
                 'ida_search', 'ida_segment', 'ida_srarea', 'ida_strlist',
                 'ida_struct', 'ida_typeinf', 'ida_ua', 'ida_xref']
 if IDA_SDK_VERSION >= 695:
-  import ida_idaapi
-  import ida_pro
-  import ida_kernwin
-  for module in modules_list:
-    globals()[module] = __import__(module)
+    import ida_idaapi
+    import ida_pro
+    import ida_kernwin
+    for module in modules_list:
+        globals()[module] = __import__(module)
 elif IDA_SDK_VERSION < 695:
-  import sys
+    import sys
 
-  ida_idaapi = idaapi
-  ida_pro = idaapi
-  ida_kernwin = idaapi
-  for module in modules_list:
-    sys.modules[module] = idaapi
+    ida_idaapi = idaapi
+    ida_pro = idaapi
+    ida_kernwin = idaapi
+    for module in modules_list:
+        sys.modules[module] = idaapi
 
 
 class Fix(object):
-  @staticmethod
-  def packagespath():
-    """Hack required in relatively old IDA linux/osx versions (around 6.4/5)
-    to successfully load python packages installed in site-packages.
+    @staticmethod
+    def packagespath():
+        """Hack required in relatively old IDA linux/osx versions (around
+        6.4/5) to successfully load python packages installed in site-packages.
 
-    IDA for linux/osx was using the machine's installed python instead of a
-    packaged version, but that version was running without using
-    site-packages. This made a user unable to install python packages and use
-    them within ida without going through quite a bit of truble, without
-    using this.
-    """
-    import sys
-    import os
-    sys.path += [os.path.join(sys.prefix, "Lib", "site-packages")]
+        IDA for linux/osx was using the machine's installed python instead of a
+        packaged version, but that version was running without using
+        site-packages. This made a user unable to install python packages and
+        use them within ida without going through quite a bit of truble,
+        without using this.
+        """
+        import sys
+        import os
+        sys.path += [os.path.join(sys.prefix, "Lib", "site-packages")]
 
-  @staticmethod
-  def actionhandlerobject():
-    """Before IDA 6.95, `action_handler_t` does not inherit from `object` and
-    that makes some python magic fail. Since 6.95 `action_handler_t` inherits
-    `object`. This fix makes reachable `action_handler_t` inherit from
-    `object` before 6.95.
-    """
-    # this makes sure we have an `object` inheriting action_handler_t
-    # regardless of version
-    if IDA_SDK_VERSION >= 695:
-      action_handler_t_object = ida_kernwin.action_handler_t
-    else:
-      class action_handler_t_object(object, ida_kernwin.action_handler_t):
-        """A base object created by `idasix.Fix.actionhandlerobject` to inherit
-        `object`."""
-        pass
+    @staticmethod
+    def actionhandlerobject():
+        """Before IDA 6.95, `action_handler_t` does not inherit from `object`
+        and that makes some python magic fail. Since 6.95 `action_handler_t`
+        inherits `object`. This fix makes reachable `action_handler_t` inherit
+        from `object` before 6.95.
+        """
+        # this makes sure we have an `object` inheriting action_handler_t
+        # regardless of version
+        if IDA_SDK_VERSION >= 695:
+            action_handler_t_obj = ida_kernwin.action_handler_t
+        else:
+            class action_handler_t_obj(object,  # noqa: N801
+                                       ida_kernwin.action_handler_t):
+                """A base object created by `idasix.Fix.actionhandlerobject` to
+                inherit `object`."""
+                pass
 
-    class action_handler_metaclass(type):
-      def __new__(cls, name, bases, dct):
-        bases = tuple(base for base in bases if base is not object)
-        return super(action_handler_metaclass, cls).__new__(cls, name, bases,
-                                                            dct)
+        class action_handler_mc(type):  # noqa: N801
+            def __new__(cls, name, bases, dct):
+                bases = tuple(base for base in bases if base is not object)
+                return super(action_handler_mc, cls).__new__(cls, name, bases,
+                                                             dct)
 
-    class action_handler_t_objprotector(action_handler_t_object):
-      """An object inheriting from ``idasix.Fix.action_handler_t_object` that
-      uses a metaclass to protect against multiple `object` inharitance. This
-      makes sure that `object` is only inherited once even when a user
-      manually inherits from it again"""
-      __metaclass__ = action_handler_metaclass
+        class action_handler_t_objprotect(action_handler_t_obj):  # noqa: N801
+            """An object inheriting from ``idasix.Fix.action_handler_t_obj`
+            that uses a metaclass to protect against multiple `object`
+            inharitance. This makes sure that `object` is only inherited once
+            even when a user manually inherits from it again"""
+            __metaclass__ = action_handler_mc
 
-    ida_kernwin.action_handler_t = action_handler_t_objprotector
+        ida_kernwin.action_handler_t = action_handler_t_objprotect
 
-  @staticmethod
-  def qtsignalslot():
-    """While pre-6.8 qt4 library pyside exposted `Qtcore.Signal` and
-    `QtCore.Slot`, new pyqt library exposes those same methods as
-    `QtCore.pyqtSignal` and `QtCore.pyqtSlot`. This fix makes sure
-    `Qtcore.Signal` and `QtCore.Slot` are always available"""
-    if IDA_SDK_VERSION >= 690:
-      QtCore.Signal = QtCore.pyqtSignal
-      QtCore.Slot = QtCore.pyqtSlot
-    elif IDA_SDK_VERSION < 690:
-      pass
+    @staticmethod
+    def qtsignalslot():
+        """While pre-6.8 qt4 library pyside exposted `Qtcore.Signal` and
+        `QtCore.Slot`, new pyqt library exposes those same methods as
+        `QtCore.pyqtSignal` and `QtCore.pyqtSlot`. This fix makes sure
+        `Qtcore.Signal` and `QtCore.Slot` are always available"""
+        if IDA_SDK_VERSION >= 690:
+            QtCore.Signal = QtCore.pyqtSignal
+            QtCore.Slot = QtCore.pyqtSlot
+        elif IDA_SDK_VERSION < 690:
+            pass
 
 
 Fix.packagespath()
